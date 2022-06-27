306f9d437f5499471cb877e98585e05ee9755d7b
awaiskhurshid-ak <awaiskhurshid.ak@gmail.com>
2022-06-27 06:28:44 +0500

    Add Project

---
+++ App.js
@@ -0,0 +1,11 @@
+import react from "react";
+import { View } from 'react-native'
+import Designbox from "./components/Designbox";
+
+const App=()=>{
+  return(
+    <Designbox/>
+  )
+}
+
+export default App;
---
+++ android/app/src/main/java/com/first_app/MainActivity.java
@@ -0,0 +1,19 @@
+package com.first_app;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+   super.onCreate(null);
+  }
+  @Override
+  protected String getMainComponentName() {
+    return "first_App";
+  }
+}
